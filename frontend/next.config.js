b65c2e0e7c2601f05bf6cc7a9821d49c94781855
Farzad <ffarzadrashidi@gmail.com>
2026-09-18 15:30:52 +0200

    Upgrade frontend dependencies for security

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,9 +5,6 @@ const nextConfig = {
     // Disable type checking on build to make the verification fast and robust
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   // Ensure we can handle heavy client-side modules like D3
   webpack: (config) => {
     config.externals.push({
